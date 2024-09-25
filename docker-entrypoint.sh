af26621ae4243d5aa6ce0d2844577753396b6fed
MountainGod2 <88257202+MountainGod2@users.noreply.github.com>
2024-09-25 17:43:55 -0600

    feat: refactored docker build to improve speed and reduce size (#26)
    * feat: refactored docker build to improve speed and reduce size of final image
    
    * refactor: update docker-entrypoint.sh to print command before execution

---
+++ docker-entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+# If the first argument is a flag, assume we want to run cbmetrics
+if [ "${1#-}" != "$1" ] || [ -z "$(command -v "$1")" ]; then
+  set -- chaturbate_poller "$@"
+fi
+
+# Print the command to the console and execute it with the arguments passed to the entrypoint
+echo "Running command:" "$@"
+exec "$@"
