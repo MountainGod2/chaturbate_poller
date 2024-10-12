a3712d734349dffc6550a913dd59ff4f54f744e9
MountainGod2 <admin@reid.ca>
2024-10-11 20:07:18 -0600

    test: update tests and add HttpStatusCode enum to test_backoff_handlers.py

--- tests/test_backoff_handlers.py
+++ tests/test_backoff_handlers.py
@@ -8,6 +8,7 @@ from httpx import (
     Response,
 )
 
+from chaturbate_poller.constants import HttpStatusCode
 from chaturbate_poller.exceptions import PollingError
 from chaturbate_poller.utils import ChaturbateUtils
 
@@ -38,7 +39,7 @@ class TestBackoffHandlers:
     def test_giveup_handler(self, caplog: Any) -> None:
         """Test the giveup handler."""
         caplog.set_level(logging.ERROR)
-        with pytest.raises(PollingError):
+        with pytest.raises(PollingError, match="Giving up due to unhandled polling error"):
             ChaturbateUtils().giveup_handler({  # type: ignore[typeddict-item, typeddict-unknown-key]
                 "tries": 6,
                 "exception": HTTPStatusError(
@@ -48,6 +49,30 @@ class TestBackoffHandlers:
                 ),
             })
         assert "Giving up after 6 tries due to server error: Status code 500" in caplog.text
+
+    @pytest.mark.parametrize(
+        ("http_code", "error_message"),
+        [
+            (HttpStatusCode.FORBIDDEN, "Giving up due to invalid credentials"),
+            (HttpStatusCode.NOT_FOUND, "Giving up due to invalid username"),
+            (HttpStatusCode.UNAUTHORIZED, "Giving up due to invalid token"),
+        ],
+    )
+    def test_giveup_handler_no_retry(self, caplog: Any, http_code: int, error_message: str) -> None:
+        """Test the giveup handler with no retry."""
+        caplog.set_level(logging.ERROR)
+        with pytest.raises(PollingError, match=error_message):
+            ChaturbateUtils().giveup_handler({  # type: ignore[typeddict-item]
+                "tries": 1,
+                "exception": HTTPStatusError(
+                    message="Server Error",
+                    request=Request("GET", "https://error.url.com"),
+                    response=Response(http_code, json={"status": "Unknown error"}),
+                ),
+            })
+        assert (
+            f"Giving up after 1 tries due to server error: Status code {http_code}" in caplog.text
+        )
 
     def test_giveup_handler_no_exception(self, caplog: Any) -> None:
         """Test the giveup handler with no exception."""
