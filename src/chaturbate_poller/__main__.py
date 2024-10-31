42fa705a4b64a2b0e9364066a966a8e430619340
MountainGod2 <admin@reid.ca>
2024-10-31 16:55:02 -0600

    refactor: Update main module import in __main__.py

--- src/chaturbate_poller/__main__.py
+++ src/chaturbate_poller/__main__.py
@@ -1,6 +1,6 @@
 """Main module for the Chaturbate Poller."""
 
-from chaturbate_poller.main import main  # pragma: no cover
+from chaturbate_poller.main import cli  # pragma: no cover
 
 if __name__ == "__main__":  # pragma: no cover
-    main()
+    cli()
