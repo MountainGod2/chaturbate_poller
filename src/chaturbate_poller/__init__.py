95d4fcd483c2a205d3dae6eb77e6d599271b33ec
MountainGod2 <admin@reid.ca>
2024-11-02 10:02:55 -0600

    fix: Update package description and add ChaturbateClient export

--- src/chaturbate_poller/__init__.py
+++ src/chaturbate_poller/__init__.py
@@ -3,6 +3,8 @@
 # Read version from installed package
 from importlib.metadata import version
 
+from chaturbate_poller.chaturbate_client import ChaturbateClient
+
 __version__ = version("chaturbate_poller")
 __author__ = "MountainGod2"
 __author_email__ = "admin@reid.ca"
@@ -10,8 +12,8 @@ __maintainer__ = "MountainGod2"
 __maintainer_email__ = "admin@reid.ca"
 __license__ = "MIT"
 __url__ = "https://github.com/MountainGod2/chaturbate_poller"
-__description__ = "Poller for the Chaturbate events API."
+__description__ = "Python library for interacting with the Chaturbate Events API."
 
 
-__all__ = [""]
+__all__ = ["ChaturbateClient"]
 """List[str]: The package exports."""
