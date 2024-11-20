976a4c1e2724f83c7bb0e77e969f6eb329dcd19a
MountainGod2 <admin@reid.ca>
2024-11-19 17:06:06 -0700

    fix: update default command in docker entrypoint to include python3 execution

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,7 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
-# If the first argument is a flag (starts with '-') or is empty, assume the default command is `chaturbate_poller`
+# If the first argument is a flag (starts with '-') or is empty, assume the default command is `python3 -m chaturbate_poller start`
 if [ "${1#-}" != "$1" ] || [ -z "$1" ]; then
   set -- python3 -m chaturbate_poller start "$@"
 fi
