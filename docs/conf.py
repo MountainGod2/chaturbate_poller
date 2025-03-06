c4a5d1b275275c9672093be46dbd8e048823bb30
MountainGod2 <admin@reid.ca>
2025-03-05 21:18:50 -0700

    refactor(docs): add type hints for extensions, version, release, autoapi_dirs, and exclude_patterns

--- docs/conf.py
+++ docs/conf.py
@@ -5,16 +5,16 @@ from chaturbate_poller import __version__
 project = "chaturbate_poller"
 project_copyright = "2024, MountainGod2"
 author = "MountainGod2"
-extensions = [
+extensions: list[str] = [
     "myst_nb",
     "autoapi.extension",
     "sphinx.ext.napoleon",
     "sphinx.ext.viewcode",
 ]
-version = __version__
-release = __version__
+version: str = __version__
+release: str = __version__
 napoleon_google_docstring = True
 napoleon_numpy_docstring = False
-autoapi_dirs = ["../src"]
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+autoapi_dirs: list[str] = ["../src"]
+exclude_patterns: list[str] = ["_build", "Thumbs.db", ".DS_Store"]
 html_theme = "sphinx_rtd_theme"
