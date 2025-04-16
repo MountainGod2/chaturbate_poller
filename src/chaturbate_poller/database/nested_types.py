ed9ceaf0d89be0b6651ae17c7a883140427da5ed
MountainGod2 <admin@reid.ca>
2025-04-16 17:40:27 -0600

    style: add pragma comments to improve code coverage clarity

--- src/chaturbate_poller/database/nested_types.py
+++ src/chaturbate_poller/database/nested_types.py
@@ -2,18 +2,18 @@
 
 from __future__ import annotations
 
-import enum
-import typing
+import enum  # pragma: no cover
+import typing  # pragma: no cover
 
 # FieldValue represents a value that can be int, float, str, or bool
-type FieldValue = float | int | str | bool
+type FieldValue = float | int | str | bool  # pragma: no cover
 
-T = typing.TypeVar("T")
+T = typing.TypeVar("T")  # pragma: no cover
 # RecursiveDict[T] represents a dictionary with string keys and values that can be of type T, another RecursiveDict, or an enum.Enum  # noqa: E501
-type RecursiveDict[T] = dict[str, T | dict[str, "RecursiveDict[T]"] | enum.Enum]
+type RecursiveDict[T] = dict[str, T | dict[str, "RecursiveDict[T]"] | enum.Enum]  # pragma: no cover
 
 # NestedDict is a RecursiveDict where values are restricted to FieldValue
-type NestedDict = RecursiveDict[FieldValue]
+type NestedDict = RecursiveDict[FieldValue]  # pragma: no cover
 
 # FlattenedDict is a dictionary with string keys and FieldValue values
-type FlattenedDict = dict[str, FieldValue]
+type FlattenedDict = dict[str, FieldValue]  # pragma: no cover
